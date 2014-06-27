a511c1698b470b9d075cac294f175eddd8da4a1a
Corey Sciuto <csciuto@constantcontact.com>
2014-06-27 15:38:53 -0600

    Well, that's hello world

--- lcwdb-webapp/src/main/java/sciuto/corey/lcwdb/MainConfiguration.java
+++ lcwdb-webapp/src/main/java/sciuto/corey/lcwdb/MainConfiguration.java
@@ -2,13 +2,19 @@ package sciuto.corey.lcwdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableAutoConfiguration
-@EnableWebMvc
 @ComponentScan
-public class MainConfiguration {
+public class MainConfiguration extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        
+        return application.sources(MainConfiguration.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainConfiguration.class, args);
